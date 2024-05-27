190cc6336167f39c7367f1029bbdb70cbd83e9c9
yusuf salim Ozbek <Yusufsalim_4532@hotmail.com>
2024-05-27 19:31:10 +0300

    Yeni proje

--- IsmailHilmiAdiguzelProje/Pages/Privacy.cshtml.cs
+++ IsmailHilmiAdiguzelProje/Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace IsmailHilmiAdiguzel.Pages
+namespace IsmailHilmiAdiguzelProje.Pages
 {
     public class PrivacyModel : PageModel
     {
