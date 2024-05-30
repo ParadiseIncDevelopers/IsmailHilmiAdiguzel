e0e1f630b406933e90fca1aa7a69e58b75645ca3
yusuf salim Ozbek <Yusufsalim_4532@hotmail.com>
2024-05-30 15:26:22 +0300

    Modeller eklendi.

---
+++ IsmailHilmiAdiguzelProje/Models/Offer.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IsmailHilmiAdiguzelProje.Models
+{
+    [Serializable]
+    public class Offer
+    {
+        [JsonPropertyName("id")]
+        public string? Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+        [JsonPropertyName("preview_url")]
+        public string? Url { get; set; }
+    }
+
+    [Serializable]
+    public class MainData
+    {
+        [JsonPropertyName("data")]
+        public Dictionary<string, Dictionary<string, Offer>>? Data { get; set; }
+    }
+
+    [Serializable]
+    public class MainDataResponse
+    {
+        [JsonPropertyName("status")]
+        public int Status { get; set; }
+
+        [JsonPropertyName("httpStatus")]
+        public int HttpStatus { get; set; }
+
+        [JsonPropertyName("data")]
+        public MainData? Data { get; set; }
+
+        [JsonPropertyName("errors")]
+        public List<object>? Errors { get; set; }
+
+        [JsonPropertyName("errorMessage")]
+        public object? ErrorMessage { get; set; }
+    }
+
+    [Serializable]
+    public class MainDataObject
+    {
+        [JsonPropertyName("request")]
+        public Dictionary<string, object>? Request { get; set; }
+
+        [JsonPropertyName("response")]
+        public MainDataResponse? Response { get; set; }
+    }
+}
---
+++ IsmailHilmiAdiguzelProje/Models/OfferImage.cs
@@ -0,0 +1,98 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IsmailHilmiAdiguzelProje.Models
+{
+    [Serializable]
+    public class OfferImage
+    {
+        [JsonPropertyName("id")]
+        public string? Id { get; set; }
+
+        [JsonPropertyName("offer_id")]
+        public string? Offer_id { get; set; }
+
+        [JsonPropertyName("display")]
+        public string? Display { get; set; }
+
+        [JsonPropertyName("filename")]
+        public string? Filename { get; set; }
+
+        [JsonPropertyName("size")]
+        public string? Size { get; set; }
+
+        [JsonPropertyName("status")]
+        public string? Status { get; set; }
+
+        [JsonPropertyName("type")]
+        public string? Type { get; set; }
+
+        [JsonPropertyName("width")]
+        public string? Width { get; set; }
+
+        [JsonPropertyName("height")]
+        public string? Height { get; set; }
+
+        [JsonPropertyName("code")]
+        public object Code { get; set; }
+
+        [JsonPropertyName("flash_vars")]
+        public object Flash_vars { get; set; }
+
+        [JsonPropertyName("interface")]
+        public string? Interface { get; set; }
+
+        [JsonPropertyName("account_id")]
+        public object Account_id { get; set; }
+
+        [JsonPropertyName("is_private")]
+        public string? Is_private { get; set; }
+
+        [JsonPropertyName("created")]
+        public string? Created { get; set; }
+
+        [JsonPropertyName("modified")]
+        public string? Modified { get; set; }
+
+        [JsonPropertyName("url")]
+        public string? Url { get; set; }
+
+        [JsonPropertyName("thumbnail")]
+        public string? Thumbnail { get; set; }
+    }
+
+    [Serializable]
+    public class ImageData
+    {
+        [JsonPropertyName("data")]
+        public Dictionary<string, Dictionary<string, OfferImage>>? OfferImage { get; set; }
+    }
+
+    [Serializable]
+    public class ImageDataResponse
+    {
+        [JsonPropertyName("status")]
+        public int Status { get; set; }
+
+        [JsonPropertyName("httpStatus")]
+        public int HttpStatus { get; set; }
+
+        [JsonPropertyName("data")]
+        public ImageData? Data { get; set; }
+
+        [JsonPropertyName("errors")]
+        public List<object>? Errors { get; set; }
+
+        [JsonPropertyName("errorMessage")]
+        public object? ErrorMessage { get; set; }
+    }
+
+    [Serializable]
+    public class ImageDataObject
+    {
+        [JsonPropertyName("request")]
+        public Dictionary<string, object>? Request { get; set; }
+
+        [JsonPropertyName("response")]
+        public ImageDataResponse? Response { get; set; }
+    }
+}
