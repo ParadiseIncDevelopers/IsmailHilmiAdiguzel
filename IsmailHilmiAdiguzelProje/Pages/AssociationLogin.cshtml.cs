b13128325b4abecd35c94f98cbb4f3d4244514b9
Mehmet Emirhan Korkmaz <korkmazmehmetemirhan@gmail.com>
2024-05-30 19:04:46 +0300

    2  tane sayfa üretildi

---
+++ IsmailHilmiAdiguzelProje/Pages/AssociationLogin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IsmailHilmiAdiguzelProje.Pages
+{
+    public class AssociationLoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
