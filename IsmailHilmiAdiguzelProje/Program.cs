5978e1090671ef25cbc95bd769af52203e505079
yusuf salim Ozbek <Yusufsalim_4532@hotmail.com>
2024-06-10 10:43:23 +0300

    Program cs de ise bazı builder değişiklikleri yapıldı.

--- IsmailHilmiAdiguzelProje/Program.cs
+++ IsmailHilmiAdiguzelProje/Program.cs
@@ -24,11 +24,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
 }
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
-
 app.Run();
