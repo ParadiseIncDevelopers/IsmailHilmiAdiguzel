c9db6a0249c1c6f94e80dd6d505d964be87e86aa
yusuf salim Ozbek <Yusufsalim_4532@hotmail.com>
2024-06-11 12:09:20 +0300

    Yeni adres modeli eklendi

---
+++ IsmailHilmiAdiguzelProje/Models/AddressModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IsmailHilmiAdiguzelProje.Models
+{
+    [Serializable]
+    public class UserAddress
+    {
+        [JsonPropertyName("id")]
+        public int id { get; set; }
+        [JsonPropertyName("province")]
+        public string? province { get; set; }
+        [JsonPropertyName("city")]
+        public string? city { get; set; }
+        [JsonPropertyName("district")]
+        public string? district { get; set; }
+        [JsonPropertyName("neighbourhood")]
+        public string? neighbourhood { get; set; }
+    }
+}
