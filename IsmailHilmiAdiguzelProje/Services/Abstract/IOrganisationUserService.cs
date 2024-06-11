e3c90e15f6d057363e4098f92cd508dbae337d4b
yusuf salim Ozbek <Yusufsalim_4532@hotmail.com>
2024-06-11 12:11:59 +0300

    Yeni bir service eklendi.

---
+++ IsmailHilmiAdiguzelProje/Services/Abstract/IOrganisationUserService.cs
@@ -0,0 +1,13 @@
+﻿using IsmailHilmiAdiguzelProje.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IsmailHilmiAdiguzelProje.Services.Abstract
+{
+    public interface IOrganisationUserService
+    {
+        Task<IActionResult> AddOrganisation(OrganisationUser organisation);
+        Task<IActionResult> EditOrganisation(Guid id, OrganisationUser organisation);
+        Task<IActionResult> DeleteOrganisation(Guid id);
+        Task<IActionResult> ConnectToOrganisation(string userEmail, string userPassword);
+    }
+}
---
+++ IsmailHilmiAdiguzelProje/Services/Concrete/OrganisationUserService.cs
@@ -0,0 +1,32 @@
+﻿using IsmailHilmiAdiguzelProje.Models;
+using IsmailHilmiAdiguzelProje.Services.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IsmailHilmiAdiguzelProje.Services.Concrete
+{
+    public class OrganisationUserService(MySQLDataContext dataContext) : IOrganisationUserService
+    {
+        private readonly MySQLDataContext _dataContext = dataContext;
+
+        public async Task<IActionResult> AddOrganisation(OrganisationUser organisation)
+        {
+            await _dataContext.organisations.AddAsync(organisation);
+            return new JsonResult(organisation);
+        }
+
+        public Task<IActionResult> ConnectToOrganisation(string userEmail, string userPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> DeleteOrganisation(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> EditOrganisation(Guid id, OrganisationUser organisation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
