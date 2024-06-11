2faca2c1c63ce1a60845a300d451639f74bdc56c
yusuf salim Ozbek <Yusufsalim_4532@hotmail.com>
2024-06-11 12:10:37 +0300

    JSON api response lar için yeni bir interface eklendi.

---
+++ IsmailHilmiAdiguzelProje/Interfaces/IJsonResponseGetter.cs
@@ -0,0 +1,20 @@
+﻿using IsmailHilmiAdiguzelProje.Models;
+using System.Text.Json.Serialization;
+
+namespace IsmailHilmiAdiguzelProje.Interfaces
+{
+    public interface IJsonResponseGetter<T>
+    {
+        Dictionary<string, object>? Request { get; set; }
+        T? Response { get; set; }
+    }
+
+    public interface IJsonDataObjectGetter<T> 
+    {
+        public int Status { get; set; }
+        public int HttpStatus { get; set; }
+        public T? Data { get; set; }
+        public List<object>? Errors { get; set; }
+        public object? ErrorMessage { get; set; }
+    }
+}
