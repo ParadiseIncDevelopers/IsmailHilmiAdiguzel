f755f701f8cef3e7b65d1f6be8282d0eb9231629
yusuf salim Ozbek <Yusufsalim_4532@hotmail.com>
2024-06-11 12:11:40 +0300

    Click counter modeli tekrar düzenleniyor.

---
+++ IsmailHilmiAdiguzelProje/Models/UserClickCounter.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace IsmailHilmiAdiguzelProje.Models
+{
+    [Serializable]
+    public class UserClickCounter
+    {
+        [Key]
+        [JsonPropertyName("id")]
+        public int id { get; set; }
+
+        [JsonPropertyName("click_website")]
+        public string? click_website { get; set; }
+
+        [JsonPropertyName("click_date")]
+        public DateTime click_date { get; set; }
+
+        [JsonPropertyName("click_name")]
+        public string? click_name { get; set; }
+    }
+}
